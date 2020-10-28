c770642dacf42c028c03a62b46e3146b7692cb0a
alexFlores9 <aleexflors123@gmail.com>
2020-10-27 23:32:54 -0600

    primera parte producto, inicio tb_usuario

--- app/src/main/java/com/example/mycrudsql/Setting_VAR.java
+++ app/src/main/java/com/example/mycrudsql/Setting_VAR.java
@@ -3,4 +3,7 @@ package com.example.mycrudsql;
 public class Setting_VAR {
 
    public static final String URL_guardar_categoria = "http://192.168.43.105/service2020/guardar_categorias.php";
+   public static final String URL_consultaAllCategorias = "http://192.168.43.105/service2020/buscar_Categorias.php";
+   public static final String URL_registrar_productos = "http://192.168.43.105/service2020/guardar_producto.php";
+
 }
